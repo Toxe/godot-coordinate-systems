aa9304fc499a9ea0e1319235ab66541011b03fba
Toxe <toxe@toxe.net>
2026-01-16 11:17:19 +0100

    Hide label instead of clearing its text

--- CoordinateSystems/scenes/blackboard.gd
+++ CoordinateSystems/scenes/blackboard.gd
@@ -8,16 +8,18 @@ var points: PackedVector2Array
 
 func _ready() -> void:
     info_label.text = "%s\nsize: %s\nscale: %s" % [name, Format.format_size(size), Format.format_size(scale)]
+    coords_label.visible = false
 
 
 func _on_mouse_exited() -> void:
-    coords_label.text = ""
+    coords_label.visible = false
 
 
 func _on_gui_input(event: InputEvent) -> void:
     var mouse_motion_event: InputEventMouseMotion = event as InputEventMouseMotion
     if mouse_motion_event:
         coords_label.text = Format.format_position(mouse_motion_event.position)
+        coords_label.visible = true
         Events.mouse_moved.emit(self, mouse_motion_event.position, get_global_transform() * mouse_motion_event.position)
 
         if mouse_motion_event.button_mask == 1:
