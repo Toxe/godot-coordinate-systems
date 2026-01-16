f869d539698acba34001b815f6e1a9684229dc10
Toxe <toxe@toxe.net>
2026-01-16 10:31:11 +0100

    Remove trailing zeros from size format

--- CoordinateSystems/scripts/format.gd
+++ CoordinateSystems/scripts/format.gd
@@ -1,8 +1,13 @@
 class_name Format
 
+
 static func format_position(vec: Vector2) -> String:
     return "%.2f / %.2f" % [vec.x, vec.y]
 
 
 static func format_size(vec: Vector2) -> String:
-    return "%.1f×%.1f" % [vec.x, vec.y]
+    return "%s×%s" % [without_trailing_zeros(vec.x), without_trailing_zeros(vec.y)]
+
+
+static func without_trailing_zeros(n: float) -> String:
+    return String.num(n, 0 if step_decimals(n) == 0 else -1)
