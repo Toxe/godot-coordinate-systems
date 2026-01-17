27d9875c23afe506b02b8999c02a8fc4709ad8d3
Toxe <toxe@toxe.net>
2026-01-17 14:01:16 +0100

    Add mouse listener script to nodes

--- CoordinateSystems/scripts/mouse_listener.gd
+++ CoordinateSystems/scripts/mouse_listener.gd
@@ -1,7 +1,7 @@
-class_name Main extends Node2D
+class_name MouseListener extends Node
 
 
 func _input(event: InputEvent) -> void:
     var mouse_motion_event: InputEventMouseMotion = event as InputEventMouseMotion
     if mouse_motion_event:
-        Events.mouse_moved.emit(self, mouse_motion_event.position, get_global_transform() * mouse_motion_event.position)
+        Events.mouse_moved.emit(self, mouse_motion_event.position)
