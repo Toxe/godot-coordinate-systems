51b4be1d03b9214bb467fb50137f7bc1dbd059a8
Toxe <toxe@toxe.net>
2026-01-26 12:03:33 +0100

    Improve mouse overlay info

--- CoordinateSystems/autoload/events.gd
+++ CoordinateSystems/autoload/events.gd
@@ -2,6 +2,6 @@ extends Node
 
 @warning_ignore_start("unused_signal")
 
-signal mouse_moved(node: Node, position: Vector2)
+signal mouse_moved(control: Control, local_mouse_coords: Vector2)
 
 @warning_ignore_restore("unused_signal")
--- CoordinateSystems/scenes/mouse_overlay.gd
+++ CoordinateSystems/scenes/mouse_overlay.gd
@@ -14,10 +14,46 @@ func _draw() -> void:
         lines.clear()
 
 
-func _on_mouse_moved(node: Node, local_position: Vector2) -> void:
-    var line := "%s: %s" % [node.name, Format.format_position(local_position)]
-    var canvas_item: CanvasItem = node as CanvasItem
-    if canvas_item:
-        line += " → %s" % [Format.format_position(canvas_item.get_global_transform() * local_position)]
+func _on_mouse_moved(control: Control, local_mouse_coords: Vector2) -> void:
+    var line := append_to_line("", control.name, local_mouse_coords)
+
+    var sub_viewport: SubViewport = control.get_viewport() as SubViewport
+    if sub_viewport:
+        # control is inside a SubViewport
+        var sub_viewport_canvas_coords := control.get_global_transform_with_canvas() * local_mouse_coords
+        line = append_subviewport_info(line, sub_viewport, sub_viewport_canvas_coords)
+
+    line = append_viewport_and_canvas_info(line, control, local_mouse_coords)
     lines.append(line)
     queue_redraw()
+
+
+func append_to_line(line: String, name_or_class: String, pos: Vector2) -> String:
+    return line + "%s%s: %s" % ["" if line == "" else " → ", name_or_class, Format.format_position(pos)]
+
+
+func append_subviewport_info(line: String, sub_viewport: SubViewport, sub_viewport_canvas_coords: Vector2) -> String:
+    line = append_to_line(line, "Canvas (%s)" % [sub_viewport.name], sub_viewport_canvas_coords)
+
+    var sub_viewport_container: SubViewportContainer = sub_viewport.get_parent() as SubViewportContainer
+    if sub_viewport_container && sub_viewport_container.stretch:
+        # sub_viewport is inside a SubViewportContainer
+        sub_viewport_canvas_coords *= sub_viewport_container.stretch_shrink
+        line = append_to_line(line, sub_viewport_container.name, sub_viewport_canvas_coords)
+        line = append_viewport_and_canvas_info(line, sub_viewport_container, sub_viewport_canvas_coords)
+
+    return line
+
+
+func append_viewport_and_canvas_info(line: String, control: Control, local_control_coords: Vector2) -> String:
+    var viewport := control.get_viewport()
+    var canvas_coords := control.get_global_transform_with_canvas() * local_control_coords
+
+    if viewport is not SubViewport:
+        line = append_to_line(line, "Canvas (%s)" % [viewport.get_class()], canvas_coords)
+
+    if viewport is Window:
+        var window_coords := control.get_viewport().get_screen_transform() * canvas_coords
+        line = append_to_line(line, "%s (%s)" % [viewport.get_class(), viewport.name], window_coords)
+
+    return line
